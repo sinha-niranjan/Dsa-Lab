424dc185d9dd4c8a2e04a255916e615395a31237
niranjan kumar <niranjankumaraina@gmail.com>
2023-01-20 22:30:06 +0530

    20-01-2022

---
+++ infixToPostfix.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool operand(char);
+bool higher(char, char);
+void exp(string);
+
+void exp(string s)
+{
+    stack<char> st;
+    string result;
+    s.push_back(')');
+    st.push('(');
+    int i = 0;
+    while (!st.empty())
+    {
+        char ch = s[i];
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        {
+            result.push_back(ch);
+        }
+        else if (operand(ch))
+        {
+            if (higher(ch, st.top()) || st.top() == '(' || st.top() == ')')
+            {
+                st.push(ch);
+            }
+            else if (ch == ')')
+            {
+                while (st.top() != '(')
+                {
+                    char temp = st.top();
+                    st.pop();
+                    result.push_back(temp);
+                }
+                st.pop();
+            }
+            else
+            {
+
+                while (!higher(ch, st.top()) && st.top() != '(' && st.top() != ')') 
+                {
+                    char temp = st.top();
+                    st.pop();
+                    result.push_back(temp);
+                }
+                st.push(ch);
+            }
+        }
+        // else if (ch == '(')
+        // {
+        //     st.push('(');
+        // }
+        // else if (ch == ')')
+        // {
+        //     while (st.top() != '(')
+        //     {
+        //         char temp = st.top();
+        //         st.pop();
+        //         result.push_back(temp);
+        //     }
+        //     st.pop();
+        // }
+        i++;
+        for (auto j : result)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    for (auto i : result)
+    {
+        cout << i;
+    }
+    cout << endl;
+}
+
+bool operand(char ch)
+{
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^' || ch == '(' || ch == ')')
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
+bool higher(char ch, char top)
+{
+    if(ch == '(') return true;
+
+    if (top == '^')
+    {
+        if (ch == '(')
+            return true;
+        return false;
+    }
+    else if (top == '*' || top == '/')
+    {
+        if (ch == '^' || ch == '(')
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    else if (top == '+' || top == '-')
+    {
+        if (ch == '^' || ch == '*' || ch == '/' || ch == '(')
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
+
+int main()
+{
+
+    string s = " a+(b*c-(d/e-f)*g)*h";
+    exp(s);
+
+    return 0;
+}
+
+// a+(b*c-(d/e-f)*g)*h
